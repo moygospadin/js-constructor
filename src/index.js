a772026da4460c4bd1ef6fd0d22ee8956797996e
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-03 21:18:38 +0300

    feat: implemented template by classes, delete template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { model } from './model'
-import { templates } from './templates'
+
 import './styles/main.css'
 const $site = document.querySelector('#site')
 
 model.forEach((block) => {
-  const toHTML = templates[block.type]
-  if (toHTML) {
-    $site.insertAdjacentHTML('beforeend', toHTML(block))
-  }
+console.log(block)
+$site.insertAdjacentHTML('beforeend', block.toHTML())
+
 })
